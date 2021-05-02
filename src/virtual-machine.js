3d597d77776fe9bd14e8f6a69b56a4e79265c356
GarboMuffin <weber_t@mailbox.org>
2021-05-01 21:33:57 -0500

    Refuse to serialize an empty VM

--- src/virtual-machine.js
+++ src/virtual-machine.js
@@ -540,6 +540,9 @@ class VirtualMachine extends EventEmitter {
      * @return {string} Serialized state of the runtime.
      */
     toJSON () {
+        if (this.runtime.targets.length === 0) {
+            throw new Error('Will not serialize empty VM');
+        }
         const sb3 = require('./serialization/sb3');
         return StringUtil.stringify(sb3.serialize(this.runtime));
     }
