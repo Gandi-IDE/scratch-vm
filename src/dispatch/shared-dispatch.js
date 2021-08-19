f8a901b33dca9fc975efd73d2da982a8826529a8
Thomas Weber <weber_t@mailbox.org>
2021-08-18 19:22:34 -0500

    Fix DataCloneError when remote extension throws error in Firefox

--- src/dispatch/shared-dispatch.js
+++ src/dispatch/shared-dispatch.js
@@ -204,7 +204,7 @@ class SharedDispatch {
             } else {
                 promise.then(
                     result => worker.postMessage({responseId: message.responseId, result}),
-                    error => worker.postMessage({responseId: message.responseId, error})
+                    error => worker.postMessage({responseId: message.responseId, error: `${error}`})
                 );
             }
         }
