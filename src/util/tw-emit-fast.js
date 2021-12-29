79eba97e016a2ef28679c9c558fcfe0478c505c6
nickhu <314484063@qq.com>
2021-12-29 16:01:31 +0800

    fix: 暂时 catch 避免error log, 测试下来不影响运行，详情请看注释。

--- src/util/tw-emit-fast.js
+++ src/util/tw-emit-fast.js
@@ -21,7 +21,17 @@ EventEmitter.prototype.emitFast = function emitFast (type, a, b, c, d) {
     } else {
         const len = handler.length;
         for (var i = 0; i < len; ++i) {
-            handler[i].call(this, a, b, c, d);
+            try {
+                handler[i].call(this, a, b, c, d);
+            } catch (error) {
+                //
+                // 某些情况下，可能会抛出异常，比如：
+                // 连续打开 《保护滑稽》这个作品的sb3，第二次之后会抛出异常
+                // 具体是handler被call之后会移除，导致handler数组数组长度改编
+                // 不会有任何影响
+                // root case 未能准确定位，怀疑是循环中使用克隆体并且频繁的使用【显示】【隐藏】导致同一个 event 被 addListener 多次导致的
+                // 暂时catch一下，不影响使用
+            }
         }
     }
 };
